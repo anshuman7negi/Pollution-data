ab7aa6a2ef776937f99bc481eebd75ec4ab90050
anshuman7negi <anshumannegi777@gmail.com>
2023-09-05 12:56:33 +0530

    Displayed countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,17 +5,31 @@ import { getData } from '../Redux/PollutionSlice';
 const Countries = () => {
   const dispatch = useDispatch();
   const { data, isLoading } = useSelector((state) => state.pollution);
-  console.log(isLoading);
-  console.log(data);
+
   useEffect(() => {
     dispatch(getData());
   }, [dispatch]);
 
-  return (
-    <div>
-      hellow
-    </div>
-  );
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else {
+    content = (
+      <div className="countriesContainer">
+        {data.map((item) => (
+          <div className="card" key={item.name.common}>
+            <p>arrow</p>
+            <img src={item.flags.png} alt="" />
+            <h3>{item.name.common}</h3>
+            <h4>{item.population}</h4>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return content;
 };
 
 export default Countries;
